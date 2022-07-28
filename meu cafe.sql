31e24c843dab8119c668e8393d5a697bc2c184e9
projesilvestrini <100374678+projesilvestrini@users.noreply.github.com>
2022-07-28 16:54:20 -0300

    Add files via upload

---
+++ meu cafe.sql
@@ -0,0 +1,18 @@
+create database cafe;
+#use cafe
+create table lanche(
+ID_lanche int auto_increment not null,
+tipolanche  varchar(100) null,
+ valor double null,
+ primary  key (ID_lanche)
+ );
+ create table bebida(
+ ID_bebida int auto_increment not null,
+ tipobebida varchar(100) null,
+ valor double null,
+ primary key (ID_bebida)
+);
+create table pagamento(
+ID_pagamento int auto_increment not null,
+troco double null,
+tipo double null,
